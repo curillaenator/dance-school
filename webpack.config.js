aa8a755acfa4813d5a2b382b7d0e4a62064a374a
Kirill <curillaenator@gmail.com>
2023-01-05 12:59:51 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
